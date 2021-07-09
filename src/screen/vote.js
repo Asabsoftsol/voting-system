631fcd60234e39990f7c7dfcc2496562c5ec5a87
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-09 22:55:40 +0530

    voteing page

--- src/screen/vote.js
+++ src/screen/vote.js
@@ -7,6 +7,63 @@ function Vote() {
     return (
         <main>
             <h1>Vote carefully</h1>
+
+            <div className="header">
+                <p>Heading</p>
+                <div className="container">
+                    <div className="section one">
+                        <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                        <b>Arun Pratap Singh</b>
+                    </div>
+                    <div className="section two"> 
+                    <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                    <b>Arun Pratap Singh</b>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="header">
+                <p>Heading</p>
+                <div className="container">
+                    <div className="section one">
+                        <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                        <b>Arun Pratap Singh</b>
+                    </div>
+                    <div className="section two"> 
+                    <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                    <b>Arun Pratap Singh</b>
+                    </div>
+                </div>
+            </div>
+
+            <div className="header">
+                <p>Heading</p>
+                <div className="container">
+                    <div className="section one">
+                        <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                        <b>Arun Pratap Singh</b>
+                    </div>
+                    <div className="section two"> 
+                    <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                    <b>Arun Pratap Singh</b>
+                    </div>
+                </div>
+            </div>
+
+            <div className="header">
+                <p>Heading</p>
+                <div className="container">
+                    <div className="section one">
+                        <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                        <b>Arun Pratap Singh</b>
+                    </div>
+                    <div className="section two"> 
+                    <img src="https://avatars.githubusercontent.com/u/48112517?v=4" alt="candidate" />
+                    <b>Arun Pratap Singh</b>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
