ac3bcbba816b7d8f011a3be027bd7003bcd6c3da
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-10 14:08:06 +0530

    save vote

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,17 +2,25 @@ const express = require('express')
 
 
 const app = express.Router()
-const evm = require('../model/index')
+const _evm = require('../model/index')
 
 
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { name, email, id, phone } = req.body
     console.log(phone.length)
-    if(phone.length <= 10 || phone.length >= 11){
-        return res.json({'msg':'ok'})
-    }
-    return res.json({'msg':'error'})
+    const voter = new _evm({
+        name, email, id, phone
+    })
+    await voter.save()
+        .then(() => {
+            return res.json({status: 'ok'})
+        })
+        .catch(err => {
+            console.log(err)
+            return res.json({status:'error'})
+        })
+    return res.json({ 'msg': 'error' })
 })
 
 
