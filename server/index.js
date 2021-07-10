aa57efa275b2256ba9bd6238980b3e1f94764752
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-10 13:29:41 +0530

    db connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
 
 
@@ -12,8 +13,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 
-
-mongoose.connect()
+mongoose.connect(process.env.URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(console.log('database connected'))
+.catch(err => console.log(err))
 
 
 app.use('/api',require('./controllers/index'))
