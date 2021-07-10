126162bdc65d8d6f788728a6beb7d220ca998fc5
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-10 14:49:48 +0530

    publish

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -8,19 +8,24 @@ const _evm = require('../model/index')
 
 app.post('/register', async (req, res) => {
     const { name, email, id, phone } = req.body
-    console.log(phone.length)
-    const voter = new _evm({
-        name, email, id, phone
+
+    _evm.findOne({ id }, async (err, vote) => {
+        if (!vote) {
+            const voter = new _evm({
+                name, email, id, phone
+            })
+            await voter.save()
+                .then(() => {
+                    return res.json({ status: 'ok' })
+                })
+                .catch(err => {
+                    console.log(err)
+                    return res.json({ status: 'error' })
+                })
+        }else{
+            return res.json({status:'error',msg:'already voted'})
+        }
     })
-    await voter.save()
-        .then(() => {
-            return res.json({status: 'ok'})
-        })
-        .catch(err => {
-            console.log(err)
-            return res.json({status:'error'})
-        })
-    return res.json({ 'msg': 'error' })
 })
 
 
